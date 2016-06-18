d4f2317adc793ab1aa3605cfa62cf371bf434502
John Ahrens <john@john-ahrens.com>
2016-06-18 11:41:37 -0700

    Initial Commit

---
+++ Orbits/Orbits.h
@@ -0,0 +1,19 @@
+//
+//  Orbits.h
+//  Orbits
+//
+//  Created by John Ahrens on 6/18/16.
+//  Copyright © 2016 John Ahrens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Orbits.
+FOUNDATION_EXPORT double OrbitsVersionNumber;
+
+//! Project version string for Orbits.
+FOUNDATION_EXPORT const unsigned char OrbitsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Orbits/PublicHeader.h>
+
+
